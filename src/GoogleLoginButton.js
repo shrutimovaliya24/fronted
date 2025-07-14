fa2150cd46cd7a9eb8762d43c4836977610b3e15
quizyantra <quizyantra@gmail.com>
2025-07-14 15:33:31 +0530

    first commit

--- src/GoogleLoginButton.js
+++ src/GoogleLoginButton.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
-const clientId = "433398797157-i4d0scs7n5qvkut4q9ru7b7i3v2dsaj1.apps.googleusercontent.com"; // Replace with your Google OAuth client ID
+const clientId =
+  "433398797157-i4d0scs7n5qvkut4q9ru7b7i3v2dsaj1.apps.googleusercontent.com"; // Replace with your Google OAuth client ID
 
 function GoogleLoginButton() {
   const handleSuccess = async (credentialResponse) => {
@@ -10,12 +11,20 @@ function GoogleLoginButton() {
     const idToken = credentialResponse.credential;
 
     // Send to backend
-    const res = await fetch("http://localhost:3000/webhook", {
+    // const res = await fetch("http://localhost:3000/webhook", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "text/plain" },
+    //   body: idToken,
+    // });
+    const API_URL =
+      window.location.hostname === "localhost"
+        ? "http://localhost:3000"
+        : "https://backend-git-main-shrutimovaliya24-gmailcoms-projects.vercel.app";
+    const res = await fetch(`${API_URL}/webhook`, {
       method: "POST",
       headers: { "Content-Type": "text/plain" },
       body: idToken,
     });
-
     if (res.ok) {
       alert("Webhook sent successfully!");
     } else {
