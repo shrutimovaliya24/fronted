806b2773127ce6486c9e81881e10ce3df90436ca
shrutimovaliya24 <shrutimovaliya24@gmail.com>
2025-07-15 16:27:10 +0530

    Update GoogleLoginButton.js

--- src/GoogleLoginButton.js
+++ src/GoogleLoginButton.js
@@ -16,7 +16,7 @@ function GoogleLoginButton() {
     const API_URL =
       window.location.hostname === "localhost"
         ? "http://localhost:5001"
-        : "https://your-deployed-backend.com"; // update this if deployed
+        : "https://backend-lemon-omega-65.vercel.app"; // update this if deployed
 
     try {
       const res = await fetch(`${API_URL}/auth/google`, {
